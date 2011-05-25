5976b14886d0ef4551c03aaf6ff4a7c53fec3d30
Salvador Fandiño García <salva@cpan.org>
2011-05-25 17:01:50 +0000

    Import of SALVA/Math-Int64-C_API-Sample-0.01 from CPAN.
    gitpan-cpan-distribution: Math-Int64-C_API-Sample
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SALVA/Math-Int64-C_API-Sample-0.01.tar.gz
    gitpan-cpan-author:       SALVA
    gitpan-cpan-maturity:     released

---
+++ lib/Math/Int64/C_API/Sample.pm
@@ -0,0 +1,51 @@
+package Math::Int64::C_API::Sample;
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Exporter qw(import);
+our @EXPORT_OK = qw(some_int64 uint64_sqr int64_addmul);
+
+our $VERSION = '0.01';
+
+require XSLoader;
+XSLoader::load('Math::Int64::C_API::Sample', $VERSION);
+
+
+1;
+__END__
+
+=head1 NAME
+
+Math::Int64::C_API::Sample - C API Usage sample for Math::Int64
+
+=head1 SYNOPSIS
+
+  use Math::Int64::C_API::Sample;
+
+=head1 DESCRIPTION
+
+This is a sample module, look at its source!
+
+=head2 EXPORT
+
+The sample subroutines available from this module are:
+
+  some_int64()
+  uint64_sqr(u64)
+  int64_addmul(acu, mul1, mul2)
+
+=head1 SEE ALSO
+
+L<Math::Int64>
+
+=head1 AUTHOR
+
+Salvador FandiE<ntilde>o, E<lt>sfandino@yahoo.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+The contents of this package are on the public domain.
+
+=cut
